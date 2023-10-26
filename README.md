0a513d7f9d1e28be6cadec7963ad24cb11388525
naungyehtet <naungyehtet717@gmail.com>
2023-10-26 21:14:10 +0630

    update

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
-DB_CONNECTION=sqlite
-
-php artisan migrate --seed
-
-got to "/badge-color-primaries"
+got to "/tags-input-issue"
--- app/Filament/Pages/TagsInputIssue.php
+++ app/Filament/Pages/TagsInputIssue.php
@@ -6,29 +6,15 @@ use Filament\Forms\Components\TagsInput;
 use Filament\Forms\Concerns\InteractsWithForms;
 use Filament\Forms\Contracts\HasForms;
 use Filament\Pages\Page;
+use Illuminate\Database\Eloquent\Model;
 
 class TagsInputIssue extends Page implements HasForms
 {
     use InteractsWithForms;
 
-    protected static ?string $navigationIcon = 'heroicon-o-user';
-
     protected static string $view = 'filament.pages.tags-input-issue';
 
     public ?array $tags = [];
-
-    public function mount(): void
-    {
-    }
-
-    public function getCancelButtonUrlProperty()
-    {
-        return static::getUrl();
-    }
-
-    public function submit()
-    {
-    }
 
     protected function getFormSchema(): array
     {
