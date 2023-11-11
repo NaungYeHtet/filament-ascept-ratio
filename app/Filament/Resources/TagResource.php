4dffdacaadb409a41c61b896091616ba93604c19
naungyehtet <naungyehtet717@gmail.com>
2023-11-11 21:14:48 +0630

    update

--- app/Filament/Resources/TagResource.php
+++ app/Filament/Resources/TagResource.php
@@ -3,13 +3,15 @@
 namespace App\Filament\Resources;
 
 use App\Filament\Resources\TagResource\Pages;
+use App\Filament\Resources\TagResource\RelationManagers;
 use App\Models\Tag;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
 use Filament\Resources\Concerns\Translatable;
-use App\Filament\Resources\TagResource\RelationManagers;
 use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Tables\Columns\Layout\Stack;
+use Filament\Tables\Columns\SelectColumn;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 
@@ -25,7 +27,7 @@ class TagResource extends Resource
     {
         return $form
             ->schema([
-                TextInput::make('name')
+                TextInput::make('name'),
             ]);
     }
 
@@ -33,8 +35,15 @@ class TagResource extends Resource
     {
         return $table
             ->columns([
-                TextColumn::make('name')
-                    ->searchable(),
+                Stack::make([
+                    TextColumn::make('name')
+                        ->searchable(),
+                    SelectColumn::make('status')
+                        ->options([
+                            'active' => 'Active',
+                            'inactive' => 'Inactive',
+                        ]),
+                ]),
             ])
             ->filters([
                 //
